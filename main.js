f8995ba070e9f5c6e916ccf8d76a677ed42a2243
Dawitlior <dawitlior777@gmail.com>
2022-06-09 14:54:25 +0300

    add

---
+++ main.js
@@ -0,0 +1,12 @@
+function checkTheEmail(){
+    var mailRegex =
+      /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+      if(true){
+
+      }
+      return;
+}
+
+function checkTheFirstName(){
+    var fNameRegex = 
+}
